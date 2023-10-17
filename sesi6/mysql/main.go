f0be62f372af5b336c8343037bd0d8425c9a0150
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2023-10-17 15:21:13 +0700

    seri 6

---
+++ sesi6/mysql/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("...")
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+}
